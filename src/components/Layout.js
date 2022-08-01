daa8f1624de543c3c9be36a95f77b9789c7fb8c5
Claudiu Florin Popa <claudiup_15@yahoo.com>
2022-08-01 13:23:06 +0200

    Sidebar finished

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import Sidebar from "./Sidebar"
 
 import "../assets/css/main.css"
 
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <>
-      <Navbar />
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
       <Footer />
     </>
